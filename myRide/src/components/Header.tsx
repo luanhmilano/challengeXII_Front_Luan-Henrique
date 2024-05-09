e47ed61b80cfb9be77fcae396310967df0c00b86
Luan Henrique <luanhsantos14@gmail.com>
2024-05-09 12:01:55 -0300

    fix: use regular css instead modules
    change header component

--- myRide/src/components/Header.tsx
+++ myRide/src/components/Header.tsx
@@ -1,13 +1,13 @@
 import logo from "../assets/Logo.png"
 import icon1 from "../assets/bell-icon.png"
 import icon2 from "../assets/user-avatar.png"
-import headerStyle from "./header.module.css"
+import './styles/header.css'
 
 const Header = () => {
   return (
-    <header className={headerStyle.header}>
-        <img src={logo} alt="Logo" />
-        <ul className={headerStyle.header_menu}>
+    <header>
+        <div><img src={logo} alt="Logo"/></div>
+        <ul className='header_menu'>
             <li>Home</li>
             <li>Getting a Taxi</li>
             <li>Mobile App</li>
