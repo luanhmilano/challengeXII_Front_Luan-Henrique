6ef019926b39ed8cd5196d0152d13aff6c8386ba
Luan Henrique <luanhsantos14@gmail.com>
2024-05-10 15:35:23 -0300

    feat: location form component
    call api ip info
    set user information in "your pickup" input
    alter color when focused on "your destination" input

--- myRide/src/components/Header.tsx
+++ myRide/src/components/Header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
     <header>
         <div><img src={logo} alt="Logo"/></div>
         <ul className='header_menu'>
-            <li><a href="https://compass.uol/pt/home/">Home</a></li>
-            <li><a href="https://compass.uol/pt/home/">Getting a Taxi</a></li>
-            <li><a href="https://compass.uol/pt/home/">Mobile App</a></li>
-            <li><a href="https://compass.uol/pt/home/">Contact Us</a></li>
+            <li><a href="https://compass.uol/pt/home/" target="_blank">Home</a></li>
+            <li><a href="https://compass.uol/pt/home/" target="_blank">Getting a Taxi</a></li>
+            <li><a href="https://compass.uol/pt/home/" target="_blank">Mobile App</a></li>
+            <li><a href="https://compass.uol/pt/home/" target="_blank">Contact Us</a></li>
         </ul>
         <div className='header-icons'>
             <Bell size={32} color="white" />
