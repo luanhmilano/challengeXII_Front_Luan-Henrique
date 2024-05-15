5f12f25ca9b1552cb6366e21828530a7b90b8fc9
Luan Henrique <luanhsantos14@gmail.com>
2024-05-15 17:39:05 -0300

    feat: countries select list
    componentize country checkbox input
    make a request on ibge api for countries

---
+++ myRide/src/components/section3/Country.tsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+interface paises {
+    nome: {
+        abreviado: string
+    }
+}
+
+const Country = () => {
+
+    const [countries, setCountries] = useState<string[] | null>(null)
+
+  useEffect(() => {
+        const fetchCountry = async () => {
+            try {
+                const response = await axios.get("https://servicodados.ibge.gov.br/api/v1/paises/{paises}");
+
+                const countries = new Set<string>()
+                response.data.map((item: paises) => {
+                    countries.add(item.nome.abreviado)
+                })
+
+                const sortCountries = Array.from(countries).sort()
+
+                setCountries(sortCountries)
+
+            } catch (error) {
+                console.error('Erro ao buscar países: ', error)
+            }
+        }
+        fetchCountry()
+}, [])
+
+  return (
+        <fieldset>
+                <legend>Country</legend>
+                <select id="countries">
+                    {countries?.map((item, index) => (
+                        <option key={"country" + index} value={item}>
+                            {item}
+                        </option>
+                    ))}
+                </select>
+        </fieldset>
+  )
+}
+
+export default Country
