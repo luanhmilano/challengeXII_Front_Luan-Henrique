47ad2db1c3a2f025c574d5dc7de1daaa1ed2cae6
Luan Henrique <luanhsantos14@gmail.com>
2024-05-15 17:36:49 -0300

    feat: create driver schema
    a zod schema based on driver schema

---
+++ myRide/src/schema/driverSchema.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod'
+
+export const driverSchema = z.object({
+    firstName: z.string().min(1, "Invalid name"),
+    lastName: z.string().min(1, "Invalid last name"),
+    email: z.string().min(1, "Invalid email").email("Invalid email"),
+    city: z.string().min(1, "Invalid city"),
+    referralCode: z.string().min(1, "Invalid code"),
+    carType: z.string({ invalid_type_error: "Select a vehicle type." })
+})
+
+export type DriverSchema = z.infer<typeof driverSchema>
