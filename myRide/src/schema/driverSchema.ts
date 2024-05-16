14e24f675d583041cbe2c0244ecc2e72633396c7
Luan Henrique <luanhsantos14@gmail.com>
2024-05-16 16:57:03 -0300

    feat: add country and car type to zod schema

--- myRide/src/schema/driverSchema.ts
+++ myRide/src/schema/driverSchema.ts
@@ -4,8 +4,12 @@ export const driverSchema = z.object({
     firstName: z.string().min(1, "Invalid name"),
     lastName: z.string().min(1, "Invalid last name"),
     email: z.string().min(1, "Invalid email").email("Invalid email"),
+    country: z.string().refine((field) => field !== "", {
+        message: "Invalid country"
+    }),
     city: z.string().min(1, "Invalid city"),
     referralCode: z.string().min(1, "Invalid code"),
+    carType: z.string({ invalid_type_error: "Select a vehicle type." })
 })
 
 export type DriverSchema = z.infer<typeof driverSchema>
