dbc59410051b4a9627d207398d05abc4085acd1e
Luan Henrique <luanhsantos14@gmail.com>
2024-05-17 17:56:00 -0300

    feat: create loader component

---
+++ myRide/src/components/section3/Loader.tsx
@@ -0,0 +1,10 @@
+import { SpinnerGap } from "@phosphor-icons/react" 
+import './styles/loader.css'
+
+const Loader = () => {
+  return (
+        <SpinnerGap size={32} color="#FBA403" weight="bold" className="loader" />
+  )
+}
+
+export default Loader
