a1066f2aaff5660d433b79b708d5f831b9d8a92f
Luan Henrique <luanhsantos14@gmail.com>
2024-05-17 17:51:23 -0300

    refactor: alter pages directory

--- myRide/src/pages/HomePage.tsx
+++ myRide/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Section1 from "../components/section1/Section1"
 import Section2 from "../components/section2/Section2"
-import Section3 from "../components/section3/Section3"
+import DriverForm from "../components/section3/DriverForm"
 
 const HomePage = () => {
   return (
@@ -10,7 +10,7 @@ const HomePage = () => {
       <Header />
       <Section1 />
       <Section2 />
-      <Section3 />
+      <DriverForm />
       <Footer />
     </>
   )
