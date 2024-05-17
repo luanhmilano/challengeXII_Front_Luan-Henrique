f4d35c71bb8b152071a745bb5ceac4403abe0994
Luan Henrique <luanhsantos14@gmail.com>
2024-05-17 17:56:34 -0300

    feat: create sucess component
    replace form when new driver is accepted

---
+++ myRide/src/components/section3/Sucess.tsx
@@ -0,0 +1,13 @@
+import { CheckCircle } from "@phosphor-icons/react"
+import './styles/section3.css'
+
+const Sucess = () => {
+  return (
+    <div className="sucess">
+      <CheckCircle size={200} weight="fill" color="#4CAF50" />
+      <h1>Welcome, Driver!</h1>
+    </div>
+  )
+}
+
+export default Sucess
